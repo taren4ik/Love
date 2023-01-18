ac2b36023bd99bfca747460df2aa6a3d4fd75249
Dmitry Permyakov <taren4ik@mail.ru>
2023-01-18 22:42:16 +1000

    Сделал вывод возраста в подробной информации о профайле.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
@@ -10,10 +11,12 @@ from .forms import CommentForm
 COUNT_PROFILES = 10
 
 
-# def profile_detail(request, profile_id):
-#     template = "profiles/index.html"
-#     return render(request, template)
-@login_required
+def get_age(args):
+    born = User.objects.get(id=args)
+    age = datetime.datetime.now().year - born.year
+    return age
+
+
 def index(request):
     template = 'profiles/index.html'
     profile_list = User.objects.select_related("category")
@@ -27,19 +30,53 @@ def index(request):
     return render(request, template, context)
 
 
+def category_profiles(request, slug):
+    template = "profiles/profile_list.html"
+    category = get_object_or_404(Category, slug=slug)
+    paginator = Paginator(category.users.all(), COUNT_PROFILES)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "category": category,
+        "page_obj": page_obj,
+    }
+    return render(request, template, context)
+
+
 def profile_detail(request, profile_id):
     template = 'profiles/profile_detail.html'
     profile = get_object_or_404(User.objects.select_related('name'),
                                 id=profile_id)
     comments = Comment.objects.filter(author_id=profile_id)
+    age = get_age(profile_id)
     form = CommentForm(request.FILES or None)
+    print(age)
     context = {
         "profile": profile,
         "form": form,
         "comments": comments,
+        'age': age
     }
     return render(request, template, context)
 
+
+@login_required
+def profile_delete(request, post_id):
+    profile = User.objects.get(id=post_id)
+    profile.delete()
+    return redirect("profiles:index", username=request.user)
+
+
+@login_required
+def add_comment(request, post_id):
+    profile = get_object_or_404(User, id=post_id)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.profile = profile
+        comment.save()
+    return redirect(index)
 
 
 @login_required
@@ -47,14 +84,32 @@ def follow_index(request):
     template = "profile/follow.html"
     authors_ids = Follow.objects.filter(
         user=request.user).values_list('author_id', flat=True)
-    posts = User.objects.filter(author_id__in=authors_ids)
-    paginator = Paginator(posts, COUNT_PROFILES)
+    profiles = User.objects.filter(author_id__in=authors_ids)
+    paginator = Paginator(profiles, COUNT_PROFILES)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     context = {
         "page_obj": page_obj,
     }
     return render(request, template, context)
+
+
+@login_required
+def profile_follow(request, username):
+    author = get_object_or_404(User, username=username)
+    if author != request.user:
+        Follow.objects.get_or_create(user=request.user, author=author)
+    return redirect("profiles:profile_detail", author)
+
+
+@login_required
+def profile_unfollow(request, username):
+    author = get_object_or_404(User, username=username)
+    user_follow = get_object_or_404(Follow.objects,
+                                    user=request.user,
+                                    author=author)
+    user_follow.delete()
+    return redirect("profiles:profile_detail", author)
 
 
 def city(request):
